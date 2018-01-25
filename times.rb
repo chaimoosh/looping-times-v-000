71f871186452348cf51deb8c28adcbfef0f0f252
Chaim Reisman <crreisman@gmail.com>
2018-01-25 22:47:37 +0000

    automatically backed up by learn

--- times.rb
+++ times.rb
@@ -1,15 +1,15 @@
-#def using_times
+def using_times
 	#your code here
-#end
+end
 
 jewels_in_bag = 100
 
-3.times do 
-  puts "Hiding 10 stolen jewels"
-  jewels_in_bag = jewels_in_bag - 10
-  puts "Now there are only #{jewels_in_bag} jewels left to hide"
-end 
+#3.times do 
+ # puts "Hiding 10 stolen jewels"
+  #jewels_in_bag = jewels_in_bag - 10
+  #puts "Now there are only #{jewels_in_bag} jewels left to hide"
+#end 
 
-puts "We have #{jewels_in_bag} jewels still to hide"
+#puts "We have #{jewels_in_bag} jewels still to hide"
 
 
