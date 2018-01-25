f6bfc8b2de7089c0c128daa2c1cec3688f40865b
Chaim Reisman <crreisman@gmail.com>
2018-01-25 22:46:52 +0000

    automatically backed up by learn

--- times.rb
+++ times.rb
@@ -6,7 +6,8 @@ jewels_in_bag = 100
 
 3.times do 
   puts "Hiding 10 stolen jewels"
-  jewels_in_bag = jewels_in_bag - 10 
+  jewels_in_bag = jewels_in_bag - 10
+  puts "Now there are only #{jewels_in_bag} jewels left to hide"
 end 
 
 puts "We have #{jewels_in_bag} jewels still to hide"
